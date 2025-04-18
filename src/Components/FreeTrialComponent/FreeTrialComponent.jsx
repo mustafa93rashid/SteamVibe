54e5714574088a82cd6ad92a44ff6de7e35d151a
alaa-assas <alaa.assas1997@gmail.com>
2025-04-18 12:19:19 +0300

    add free trial section folder and some html tag

---
+++ src/Components/FreeTrialComponent/FreeTrialComponent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./FreeTrialComponent.css";
+import TitleComponent from "./../../Components/TitleComponent/TitleComponent";
+
+export default function FreeTrialComponent() {
+  return (
+    <section className="px-161 pb-150">
+      <div className="AA-free-trial mb-80">
+        <TitleComponent
+          title={"Start your free trial today!"}
+          descrption={
+            "This is a clear and concise call to action that encourages users to sign up for a free trial of StreamVibe."
+          }
+        />
+        <button className="AA-ask-btn fs-18 fw-600">Start a Free Trail</button>
+      </div>
+    </section>
+  );
+}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import CategoriesSectionComponent from '../../Components/CategoriesSectionComponent/CategoriesSectionComponent'
 import FAQSectionComponent from '../../Components/FAQSectionComponent/FAQSectionComponent'
+import FreeTrialComponent from '../../Components/FreeTrialComponent/FreeTrialComponent'
 
 export default function Home() {
   return (
     <>
       <CategoriesSectionComponent />
       <FAQSectionComponent />
+      <FreeTrialComponent />
     </>
   )
 }
