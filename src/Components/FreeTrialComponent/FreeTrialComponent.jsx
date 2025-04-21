93fe0fb05630f537bb2bd148850f28593c75d8ce
alaa-assas <alaa.assas1997@gmail.com>
2025-04-21 22:41:36 +0300

    add images of free trial and make some design on this section

--- src/Components/FreeTrialComponent/FreeTrialComponent.jsx
+++ src/Components/FreeTrialComponent/FreeTrialComponent.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import "./FreeTrialComponent.css";
 import TitleComponent from "./../../Components/TitleComponent/TitleComponent";
+import FreeTrialData from "../../Data/FreeTrialData";
 
 export default function FreeTrialComponent() {
   return (
     <section className="px-161 pb-150">
-      <div className="AA-free-trial mb-80">
-        <TitleComponent
-          title={"Start your free trial today!"}
-          descrption={
-            "This is a clear and concise call to action that encourages users to sign up for a free trial of StreamVibe."
-          }
-        />
-        <button className="AA-free-btn fs-18 fw-600">Start a Free Trail</button>
+      <div className="slider-container mb-80">
+        <div className="AA-free-trial">
+          <TitleComponent
+            title={"Start your free trial today!"}
+            descrption={
+              "This is a clear and concise call to action that encourages users to sign up for a free trial of StreamVibe."
+            }
+          />
+          <button className="AA-free-btn fs-18 fw-600">
+            Start a Free Trail
+          </button>
+        </div>
+        {FreeTrialData.map((row, index) => (
+          <div className="slider">
+            {row?.map((image, index) => (
+              <img
+                key={index}
+                src={image.src}
+                alt={image.alt}
+                className="slider-image"
+              />
+            ))}
+          </div>
+        ))}
       </div>
     </section>
   );
---
+++ src/Data/FreeTrialData.jsx
@@ -0,0 +1,82 @@
+
+import img1 from './../assets/images/FreeTrial/Image1.png';
+import img2 from './../assets/images/FreeTrial/Image2.png';
+import img3 from './../assets/images/FreeTrial/Image3.png';
+import img4 from './../assets/images/FreeTrial/Image4.png';
+import img5 from './../assets/images/FreeTrial/Image5.png';
+import img6 from './../assets/images/FreeTrial/Image6.png';
+import img7 from './../assets/images/FreeTrial/Image7.png';
+import img8 from './../assets/images/FreeTrial/Image8.png';
+import img9 from './../assets/images/FreeTrial/Image9.png';
+import img10 from './../assets/images/FreeTrial/Image10.png';
+import img11 from './../assets/images/FreeTrial/Image11.png';
+import img12 from './../assets/images/FreeTrial/Image12.png';
+import img13 from './../assets/images/FreeTrial/Image13.png';
+import img14 from './../assets/images/FreeTrial/Image14.png';
+import img15 from './../assets/images/FreeTrial/Image15.png';
+import img16 from './../assets/images/FreeTrial/Image16.png';
+import img17 from './../assets/images/FreeTrial/Image17.png';
+import img18 from './../assets/images/FreeTrial/Image18.png';
+import img19 from './../assets/images/FreeTrial/Image19.png';
+import img20 from './../assets/images/FreeTrial/Image20.png';
+import img21 from './../assets/images/FreeTrial/Image21.png';
+import img22 from './../assets/images/FreeTrial/Image22.png';
+import img23 from './../assets/images/FreeTrial/Image23.png';
+import img24 from './../assets/images/FreeTrial/Image24.png';
+import img25 from './../assets/images/FreeTrial/Image25.png';
+import img26 from './../assets/images/FreeTrial/Image26.png';
+import img27 from './../assets/images/FreeTrial/Image27.png';
+import img28 from './../assets/images/FreeTrial/Image28.png';
+import img29 from './../assets/images/FreeTrial/Image29.png';
+import img30 from './../assets/images/FreeTrial/Image30.png';
+import img31 from './../assets/images/FreeTrial/Image31.png';
+import img32 from './../assets/images/FreeTrial/Image32.png';
+import img33 from './../assets/images/FreeTrial/Image33.png';
+import img34 from './../assets/images/FreeTrial/Image34.png';
+import img35 from './../assets/images/FreeTrial/Image35.png';
+import img36 from './../assets/images/FreeTrial/Image36.png';
+
+const FreeTrialData = [[
+  { src: img1, alt: 'Image 1' },
+  { src: img2, alt: 'Image 2' },
+  { src: img3, alt: 'Image 3' },
+  { src: img4, alt: 'Image 4' },
+  { src: img5, alt: 'Image 5' },
+  { src: img6, alt: 'Image 6' },
+  { src: img7, alt: 'Image 7' },
+  { src: img8, alt: 'Image 8' },
+  { src: img9, alt: 'Image 9' }
+  ],[
+  { src: img10, alt: 'Image 10' },
+  { src: img11, alt: 'Image 11' },
+  { src: img12, alt: 'Image 12' },
+  { src: img13, alt: 'Image 13' },
+  { src: img14, alt: 'Image 14' },
+  { src: img15, alt: 'Image 15' },
+  { src: img16, alt: 'Image 16' },
+  { src: img17, alt: 'Image 17' },
+  { src: img18, alt: 'Image 18' }
+],[
+  { src: img19, alt: 'Image 19' },
+  { src: img20, alt: 'Image 20' },
+  { src: img21, alt: 'Image 21' },
+  { src: img22, alt: 'Image 22' },
+  { src: img23, alt: 'Image 23' },
+  { src: img24, alt: 'Image 24' },
+  { src: img25, alt: 'Image 25' },
+  { src: img26, alt: 'Image 26' },
+  { src: img27, alt: 'Image 27' }
+], [
+  { src: img28, alt: 'Image 18' },
+  { src: img29, alt: 'Image 29' },
+  { src: img30, alt: 'Image 30' },
+  { src: img31, alt: 'Image 31' },
+  { src: img32, alt: 'Image 32' },
+  { src: img33, alt: 'Image 33' },
+  { src: img34, alt: 'Image 34' },
+  { src: img35, alt: 'Image 35' },
+  { src: img36, alt: 'Image 36' }
+]
+];
+
+export default FreeTrialData;
